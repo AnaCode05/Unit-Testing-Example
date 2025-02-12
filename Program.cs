abd8f3fe8b73a980219d41b04f436f65452b4387
AnaCode05 <ana.isakov05@gmail.com>
2025-02-12 10:23:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator_UnitTestingExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
